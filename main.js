dd277f0d7e67078f0acfd21b989dcf26473e8928
Rocio <rbrueda8@gmail.com>
2023-06-29 15:42:31 -0400

    Figuring Out Geocoordinates

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// require('dotenv').config();
-// console.log(process.env.APIKEY);
-
 let map;
 //used before attaining the environment variables
+let geocoder;
+let respondseDiv;
+let response;
 
 
 async function initMap() {
   //provides geographical coordinates corresponding to a location -- this is causing google maps platform to not pop up!
-  // geocoder = new google.maps.Geocoder();
+
   const { Map } = await google.maps.importLibrary("maps");
   map = new Map(document.getElementById("map"), {
     //coordinates of windsor
@@ -16,7 +16,10 @@ async function initMap() {
     mapTypeId: "terrain",
 
   });
+  geocoder = new google.maps.Geocoder();
   //create markers -- this will be added depending on businesses input of address information
+  //will go to the markersOnMap function
+  //plan= go through an arraylist which will go through all the elements in the array
   const marker = new google.maps.Marker({
     position: { lat: 42.3149, lng: -83.0364 },
     //which map we want to specify the marker
@@ -36,19 +39,58 @@ infoWindow.open(map, marker);
 }
 initMap();
 
-// //will go to function if button is clicked
+//will go to function if button is clicked
 function markersOnMap(){  
   //used to check if button was clicked 
   alert("Button clicked");
-  //add marker based on address inputted
 
+  //got address
+  // var address = document.getElementById('address').value;
+  alert(document.getElementById('address').value);
+  const coordinates = new Array(2);
+  //calls the function to convert address to coordinates
+  coordinates = geocode({ address: document.getElementById('address').value });
+  //calls function to find geocode address 
+  const marker = new google.maps.Marker({
+    position: { lat: coordinates[0], lng: coordinates[1] },
+    //which map we want to specify the marker
+    map: map,
+    label: "S", //possible way to categorize our stores?
+    title: "New location",
+    draggable: false,
+    animation: google.maps.Animation.DROP,
+    // icon: "maps png" //change the markers by colour (inserting an image)
+  });
+  //allows us to create infomration windows over the markers
+  
+  //bring this code to the async function initMap()-- markersOnMap will return "marker"
+  const infoWindow = new google.maps.InfoWindow({
+    content: "<p>This is an another window</p>",
+  });
+  infoWindow.open(map, marker);
+  //add these elements to arrayList
+
+  //add marker based on address inputted
+}
+
+//create geocoordinates to system -- modify from stackoverflow file
+function geocode(request){
+
+  geocoder.geocode( {'address': address}, function(results, status) {
+
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latitude = results.geometry.location.lat();
+      var longitude = results.geometry.location.lng();
+      alert(latitude);
+      alert(longitude)
+    } 
+  }); 
+  //returns the longitude and laitude coordinates
+  return [latitude, longitude];
 
 }
 
-// //create geocoordinates to system
-// function geocodeAddress(address){
-  
-// }
+
 
 
 
